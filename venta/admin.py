0c6006b4a09a121d008ab757edae9b1bd13b8948
Lucas Fernandez <129115917+MrLucasFernandez@users.noreply.github.com>
2023-06-28 22:18:32 -0400

    Delete venta directory

--- venta/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Categoria,Producto,Pedido,Cliente, Boleta
-
-# Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Producto)
-admin.site.register(Pedido)
-admin.site.register(Cliente)
-admin.site.register(Boleta)
